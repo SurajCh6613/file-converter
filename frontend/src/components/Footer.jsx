4248dc05aad56925dd935e6c1b8068eca94d262e
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-10 13:46:07 +0530

    Footer added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="text-center">
+        <hr />
+        <h1 className="p-2">Copyright C 2025. All right reserved by Suraj Chaudhary</h1>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
