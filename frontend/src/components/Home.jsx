80350632bafe7fb09573b2d2c36b2814dbfe3bda
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-10 13:46:42 +0530

    Integrated frontend backend connection

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaFileWord } from "react-icons/fa";
 
+import axios from "axios";
 const Home = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [message, setMessage] = useState("");
+  const [downloadError, setDownloadError] = useState("");
+
+  const handleOnChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!selectedFile) {
+      setMessage("Please select a file");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/convertfile",
+        formData,
+        {
+          responseType: "blob",
+        }
+      );
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute(
+        "download",
+        selectedFile.name.replace(/\.[^/.]+$/, "") + ".pdf"
+      );
+      
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+      setSelectedFile(null);
+      setMessage("File Converted Successfully");
+    } catch (error) {
+      console.log(error);
+      setDownloadError("Download Error",error);
+      setMessage("");
+    }
+  };
   return (
     <>
       <div className="max-w-screen-2xl mx-auto container py-3 px-6 md:px-40">
@@ -18,6 +62,7 @@ const Home = () => {
             <div className="flex flex-col items-center space-y-4">
               <input
                 type="file"
+                onChange={handleOnChange}
                 accept=".doc,.docx"
                 className="hidden"
                 id="FileInput"
@@ -27,11 +72,19 @@ const Home = () => {
                 className="w-full flex items-center justify-center px-4 py-6 bg-gray-100 text-gray-700 shadow-lg rounded-lg cursor-pointer border-blue-300 hover:bg-blue-700 hover:text-white"
               >
                 <FaFileWord className="text-3xl" />{" "}
-                <span className="text-3xl ml-2 uppercase ">Choose File</span>
+                <span className="text-3xl ml-2 uppercase ">
+                  {selectedFile ? selectedFile.name : "Choose File"}
+                </span>
               </label>
-              <button className="text-white bg-blue-500 hover:bg-blue-700 duration-300 font-bold px-4 py-2 rounded-lg">
+              <button
+                onClick={handleSubmit}
+                disabled={!selectedFile}
+                className="text-white bg-blue-500 hover:bg-blue-700 disabled:bg-gray-400 disabled:pointer-events-none cursor-pointer duration-300 font-bold px-4 py-2 rounded-lg"
+              >
                 Convert File
               </button>
+              {message && (<div className="text-green-600 text-center">{message}</div>)}
+              {downloadError && (<div className="text-red-500 text-center">{downloadError}</div>)}
             </div>
           </div>
         </div>
